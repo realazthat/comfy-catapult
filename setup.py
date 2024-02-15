c15bc46bea06a2bd9b5137055f93a06f58fa72aa
Azriel Fasten <realazthat@gmail.com>
2024-02-15 12:53:39 -0500

    Bump version to 1.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='comfy_catapult',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=['examples']),
     description='Programmatically schedule ComfyUI workflows',
     long_description=long_description,
