71fa7adb04fc36ec9a2bffc1496353ede317f92b
Azriel Fasten <realazthat@gmail.com>
2024-03-03 19:17:49 -0500

    Fix mismatch between CKPT_FILE used for workflow cache and used for workflow.

--- scripts/run-comfy-for-tests.sh
+++ scripts/run-comfy-for-tests.sh
@@ -5,9 +5,6 @@ set -e -x -v -u -o pipefail
 SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 source "${SCRIPT_DIR}/utilities/common.sh"
 
-VENV_PATH=.cache/scripts/.venv source "${PROJ_PATH}/scripts/utilities/ensure-venv.sh"
-REQS="${PROJ_PATH}/scripts/requirements-dev.txt" source "${PROJ_PATH}/scripts/utilities/ensure-reqs.sh"
-
 # COMFY_IMAGE_NAME=ghcr.io/ai-dock/comfyui:pytorch-2.1.2-py3.10-cuda-12.1.0-runtime-22.04
 COMFY_IMAGE_NAME=ghcr.io/ai-dock/comfyui:pytorch-2.2.0-py3.10-cpu-22.04
 # PROVISIONING_SCRIPT="${PWD}/scripts/provisioning-for-tests.sh"
@@ -22,21 +19,24 @@ COMFY_INSTANCE_NAME=${COMFY_INSTANCE_NAME:-comfy-test-instance}
 ################################################################################
 # Download the model checkpoint
 CKPT_URL="https://huggingface.co/stabilityai/sdxl-turbo/resolve/main/sd_xl_turbo_1.0_fp16.safetensors"
-EXPECTED_CKPT_SHA256=e869ac7d6942cb327d68d5ed83a40447aadf20e0c3358d98b2cc9e270db0da26
+CKPT_SHA256=e869ac7d6942cb327d68d5ed83a40447aadf20e0c3358d98b2cc9e270db0da26
 CKPT_DIR="${PWD}/.cache/models-for-tests"
 mkdir -p "${CKPT_DIR}"
 CKPT_FILE="${CKPT_DIR}/sd_xl_turbo_1.0_fp16.safetensors"
-wget -q --continue --show-progress -e dotbytes="4M" -O "${CKPT_FILE}" "${CKPT_URL}"
-CKPT_SHA256=$(sha256sum "${CKPT_FILE}" | awk '{print $1}')
-echo "${CKPT_SHA256} ${CKPT_FILE}" | sha256sum --check --status
-echo "${CKPT_SHA256} ${CKPT_FILE}" \
-  | tee "${CKPT_FILE}.sha256"
 
-# Check thet CKPT_SHA256 matches EXPECTED_CKPT_SHA256
-if [[ "${CKPT_SHA256}" != "${EXPECTED_CKPT_SHA256}" ]]; then
-  echo -e "${RED}The downloaded checkpoint does not match the expected sha256${NC}"
-  exit 1
+
+if ! echo "${CKPT_SHA256}  ${CKPT_FILE}" | sha256sum --check --status; then
+  echo -e "${YELLOW}Downloading checkpoint${NC}"
+  mkdir -p $(dirname "${CKPT_FILE}")
+  wget -q --continue --show-progress -e dotbytes="4M" -O "${CKPT_FILE}" "${CKPT_URL}"
+  echo "${CKPT_SHA256}  ${CKPT_FILE}" | sha256sum --check --status
+else
+  echo -e "${GREEN}Checkpoint already downloaded${NC}"
 fi
+
+chmod a+wxr "${CKPT_FILE}"
+chmod -R a+wxr "${CKPT_DIR}"
+# chown 1000:1111 "${CKPT_FILE}"
 ls -la "${CKPT_DIR}"
 ################################################################################
 
@@ -49,10 +49,6 @@ DOCKER_CKPT_FILE="${DOCKER_CKPT_DIR}/sd_xl_turbo_1.0_fp16.safetensors"
 echo -e "${YELLOW}Stopping and removing existing comfy instance${NC}"
 docker rm --force "${COMFY_INSTANCE_NAME}" || true
 echo -e "${YELLOW}Starting comfy instance${NC}"
-# Note: Must mount the checkpoint file as read-only, the permissions/owners etc.
-# won't work well inside act.
-# -v "${CKPT_FILE}:${DOCKER_CKPT_FILE}:ro" \
-
 
 docker run -d --rm \
   -e COMFYUI_PORT=41112 \
@@ -64,13 +60,10 @@ docker run -d --rm \
   --name "${COMFY_INSTANCE_NAME}" \
   "${COMFY_IMAGE_NAME}" \
   init.sh
-
-ls -la "${CKPT_DIR}"
-echo "${CKPT_SHA256} ${CKPT_FILE}" | sha256sum --check --status
 docker cp "${CKPT_FILE}" "${COMFY_INSTANCE_NAME}:${DOCKER_CKPT_FILE}"
+ls -la "${CKPT_DIR}"
 
 docker exec "${COMFY_INSTANCE_NAME}" /bin/bash -c "ls -la ${DOCKER_CKPT_DIR}"
-
 docker exec "${COMFY_INSTANCE_NAME}" /bin/bash -c "echo \"${CKPT_SHA256} ${DOCKER_CKPT_FILE}\" | sha256sum --check --status"
 
 while ! curl -s "http://localhost:41112/system_stats" > /dev/null; do
