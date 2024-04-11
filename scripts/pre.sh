119b73cb8b74688c27dec39b3dafb2e3b2a6d375
Azriel Fasten <realazthat@gmail.com>
2024-04-11 09:05:30 -0400

    Changeguard script for change detection.

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -6,6 +6,25 @@ SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 source "${SCRIPT_DIR}/utilities/common.sh"
 
 
+# This variable will be 1 when we are the ideal version in the GH action matrix.
+IDEAL="0"
+PYTHON_VERSION=$(cat .python-version)
+if [[ "${PYTHON_VERSION}" == "3.10.0" ]]; then
+  IDEAL="1"
+fi
+
+if [[ -z "${GITHUB_ACTIONS:-}" ]]; then
+  if [[ "${IDEAL}" != "1" ]]; then
+    echo -e "${RED}Somehow we are not 'ideal' outside of GH Action workflow. IDEAL is meant to be 1 when we are running the GH Action matrix configuration that matches the configuration outside of the GH Action workflow. But we are not in the GH Action workflow, yet it is not 1!${NC}"
+    exit 1
+  fi
+fi
+
+# Check that no changes occurred to files through the workflow.
+if [[ "${IDEAL}" == "1" ]]; then
+  STEP=pre bash scripts/utilities/changeguard.sh
+fi
+
 EXTRA=dev bash scripts/utilities/pin-extra-reqs.sh
 EXTRA=prod bash scripts/utilities/pin-extra-reqs.sh
 bash scripts/format.sh
@@ -15,4 +34,10 @@ bash scripts/run-all-examples.sh
 if [[ -z "${GITHUB_ACTIONS:-}" ]]; then
 	bash scripts/precommit.sh
   bash scripts/act.sh
+fi
+
+# Check that no changes occurred to files throughout pre.sh to tracked files. If
+# changes occurred, they should be staged and pre.sh should be run again.
+if [[ "${IDEAL}" == "1" ]]; then
+  STEP=post bash scripts/utilities/changeguard.sh
 fi
